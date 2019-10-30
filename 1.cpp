2613c4cf5bcf1ad513c3a5160e5145d5a2aac788
Default user <l.wandurraga@uniandes.edu.co>
2019-10-30 20:16:07 +0000

    Verificado hasta perimetro

--- 1.cpp
+++ 1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-float dia(int x);
-float per(int x);
+float dia(float x);
+float per(float x);
 
 int main(void)
 {
     std::cout<<"Ingrese el radio del circulo"<<std::endl;
     float r;
     std::cin>>r;
-    std::cout<<"El radio es"<<r<<"\n"<<"El diametro es"<<dia(r)<<"\n"<<"El perimetro es"<<per(r)<<"\n"<<std::endl;
+    std::cout<<"El radio es "<<r<<"\n"<<"El diametro es "<<dia(r)<<"\n"<<"El perimetro es"<<per(r)<<"\n"<<std::endl;
     return 0;
 }
 float dia(float x){
