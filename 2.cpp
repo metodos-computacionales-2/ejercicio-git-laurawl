c5abf27be73fee989234be6c815411911a3cc1d4
Default user <l.wandurraga@uniandes.edu.co>
2019-10-30 20:33:41 +0000

    Intento 2 con el resto de cosas

--- 2.cpp
+++ 2.cpp
@@ -1,12 +1,23 @@
+#include <iostream>
+#include <cmath>
+float pita(float x,float x2);
+
 int main(void)
 {
-    std::cout<<"Ingrese los numeros de los lados de un rectangulo"<<std::endl;
+    std::cout<<"Ingrese los numeros de los catetos"<<std::endl;
     float r;
     float r1;
     float r2;
+    string a="si";
     std::cin>>r;
     std::cin>>r1;
+    std::cout<<"Ingrese la hipotenusa"<<std::endl;
     std::cin>>r2;
-    std::cout<<"El radio es "<<r<<std::endl;
+    if(r2=pita(
+    std::cout<<"Representan los lados de un triangulo rectangulo"<<a<<std::endl;
     return 0;
+}
+float pita(float x,float x2)
+{
+    return sqrt(x*x+x2*x2)
 }
