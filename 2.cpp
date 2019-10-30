e5458cd81580163c51005640e97be86306981b83
Default user <l.wandurraga@uniandes.edu.co>
2019-10-30 20:24:24 +0000

    Primer intento

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+int main(void)
+{
+    std::cout<<"Ingrese los numeros de los lados de un rectangulo"<<std::endl;
+    float r;
+    float r1;
+    float r2;
+    std::cin>>r;
+    std::cin>>r1;
+    std::cin>>r2;
+    std::cout<<"El radio es "<<r<<std::endl;
+    return 0;
+}
